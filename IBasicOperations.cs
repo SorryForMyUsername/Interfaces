e99212dc708413314d36dd6f4e5b48153d8bd5b3
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-04-17 11:15:49 +0300

    Add files via upload

---
+++ IBasicOperations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_6
+{
+    interface IBasicOperations
+    {
+        int Sum(int x, int y);
+        int Sub(int x, int y);
+        int Multiply(int x, int multiplier);
+        int Divide(int x, int divider);
+    }
+}
---
+++ IExtendedOperations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_6
+{
+    interface IExtendedOperations
+    {
+        void Sqrt(int x);
+        void Pow(int x, int degree);
+    }
+}
