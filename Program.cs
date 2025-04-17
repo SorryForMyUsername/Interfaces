7b206a7b7caed3c93612ac5ad95e26ab5934e244
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-04-17 10:25:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass dClass = new DerivedClass();
+            dClass.Method();
+
+            IInterface iClass = dClass;
+            iClass.Method();
+
+            Console.ReadKey(true);
+        }
+    }
+}
