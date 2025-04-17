b1c8d4bfcdbcf06b05e9459a24b16187c05499ca
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-04-17 21:12:01 +0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_7
+{
+    internal class Player : IPlayable, IRecodable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Проигрывание началось");
+        }
+
+        public void Record()
+        {
+            Console.WriteLine("Запись началась");
+        }
+
+        void IPlayable.Stop()
+        {
+            Console.WriteLine("Проигрывание приостановлено");
+        }
+
+        void IRecodable.Stop()
+        {
+            Console.WriteLine("Запись приостановлена");
+        }
+
+        void IPlayable.Pause()
+        {
+            Console.WriteLine("Проигрывание поставлено на паузу");
+        }
+
+        void IRecodable.Pause()
+        {
+            Console.WriteLine("Запись поставлена на паузу");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _37_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player p = new Player();
+            IPlayable playable = p;
+            IRecodable recodable = p;
+
+            playable.Play();
+            Thread.Sleep(1000);
+            playable.Pause();
+            Thread.Sleep(300);
+            recodable.Record();
+            Thread.Sleep(500);
+            playable.Play();
+            Thread.Sleep(2000);
+            recodable.Stop();
+            Thread.Sleep(300);
+            playable.Stop();
+
+            Console.ReadKey(true);
+        }
+    }
+}
