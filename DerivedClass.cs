2087b10940b5eae27a07cd40e9d0115d6c6dc4be
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-04-17 10:49:47 +0300

    Add files via upload

---
+++ DerivedClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_3
+{
+    internal class DerivedClass : IInterface3
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Метод Method1\n");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Метод Method2\n");
+        }
+
+        public void Method3()
+        {
+            Console.WriteLine("Метод Method3\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Объект DerivedClass:");
+            DerivedClass dClass = new DerivedClass();
+            dClass.Method1();
+            dClass.Method2();
+            dClass.Method3();
+
+            Console.WriteLine("\nОбъект IInterface3:");
+            IInterface3 i3Class = dClass;
+            i3Class.Method1();
+            i3Class.Method2();
+            i3Class.Method3();
+
+            Console.WriteLine("\nОбъект IInterface2:");
+            IInterface2 i2Class = dClass;
+            i2Class.Method1();
+            i2Class.Method2();
+
+            Console.WriteLine("\nОбъект IInterface1:");
+            IInterface1 i1Class = dClass;
+            i1Class.Method1();
+
+            Console.ReadKey(true);
+        }
+    }
+}
